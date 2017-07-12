d35e0c3c7aeb485e8807be97c4838a56783faaa5
Luca Iocchi <iocchi@dis.uniroma1.it>
2017-07-12 13:34:31 +0200

    committed by iocchi from iocchi-w1 at mer 12 lug 2017, 13.34.31, CEST

--- scripts/init.py
+++ scripts/init.py
@@ -10,7 +10,7 @@ import webinit, postureinit, behaviorinit
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--pip", type=str, default=os.environ['PEPPER_IP'],
+    parser.add_argument("--pip", type=str, default='localhost',
                         help="Robot IP address. On robot or Local Naoqi: use '127.0.0.1'.")
     parser.add_argument("--pport", type=int, default=9559,
                         help="Naoqi port number")
