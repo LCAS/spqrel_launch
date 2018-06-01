e43f9045400ff90c068e8005a390c3a1b7bbbec8
Marc Hanheide <marc@hanheide.net>
2018-06-01 09:03:22 +0100

    checkout and update-all

--- update-all.sh
+++ update-all.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-for d in `find  * -name .git  | xargs -n 1 dirname`; do (cd $d; pwd; git pull); done
+# see https://stackoverflow.com/questions/1777854/git-submodules-specify-a-branch-tag/18799234#18799234
 
+# configure all submodules to actually checkout the respective branches
+git submodule foreach -q  \
+  'echo "checking out and pulling for ${name}..." ; git checkout \
+  $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master); git pull; \
+  echo "================================================================"'
 
